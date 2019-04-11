bdf607c290e69cf97831a64773376dd86814a047
Robert <r_lee94@yahoo.com>
2019-04-11 04:20:30 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,12 +9,8 @@ class Project
   
   def add_backer(backer)
     backers << backer
-    backer.backed_projects
-  
-  
+    backer.backed_projects << self
   end
-  # def back_project(project)
-  #   backed_projects << project
-  #   project.backers << self
+
   
 end
